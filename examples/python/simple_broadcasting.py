Fix broadcasting bugs and update tests/examples

examples/python/simple_broadcasting.py
@@ -10,13 +10,15 @@ def validate_and_print_results(computed, expected, operation_name):
     print("*" * 60)
     print(f"\nExpected:\n{expected}")
     print(f"\nDecrypted Result:\n{computed}")
-
-    is_match, error = onp.check_equality(computed, expected)
+s
+    is_match, error = onp.check_equality(expected, computed)
     print(f"\nMatch: {is_match}, Total Error: {error}")
-    return is_match, error
+
+    if not is_match:
+        print("\n>>> ERROR: Pattern does not match")
 
 
-def example_broadcasting():
+def example_broadcasting(order=onp.ROW_MAJOR):
     # Cryptographic setup
     mult_depth = 5
     params = CCParamsCKKSRNS()
@@ -32,7 +34,7 @@ def example_broadcasting():
     keys = cc.KeyGen()
     cc.EvalMultKeyGen(keys.secretKey)
     cc.EvalSumKeyGen(keys.secretKey)
-    onp.gen_rotation_keys(keys.secretKey, list(range(-32, 32)))
+    onp.generate_broadcast_key(keys.secretKey, (3, 5))
 
     ring_dim = cc.GetRingDimension()
     batch_size = ring_dim // 2
@@ -41,18 +43,22 @@ def example_broadcasting():
 
     scalar = 5
     vector = [10, 20, 30]
+    vector = [9.8745164, 8.72523343, 6.14485141, 5.19928817, 5.53364538]
+
+    column_vector = [[10], [20], [30]]
     matrix = [[1.0, 2.0, 3.0, 4.0, 5.0], [1.0, 2.0, 3.0, 4.0, 5.0], [1.0, 2.0, 3.0, 4.0, 5.0]]
 
     print("\nInputs")
-    print("vector_a:", scalar)
-    print("vector_b:", vector)
-    print("vector_c:", matrix)
+    print("scalar:", scalar)
+    print("vector:", vector)
+    print("column_vector:", column_vector)
+    print("matrix:", matrix)
 
     cts = onp.array(
         cc=cc,
         data=scalar,
         batch_size=batch_size,
-        order=onp.ROW_MAJOR,
+        order=order,
         mode="zero",
         fhe_type="C",
         public_key=keys.publicKey,
@@ -62,27 +68,17 @@ def example_broadcasting():
         cc=cc,
         data=vector,
         batch_size=batch_size,
-        order=onp.ROW_MAJOR,
+        order=order,
         mode="zero",
         fhe_type="C",
         public_key=keys.publicKey,
     )
 
-    cta_rm = onp.array(
+    ctv_col = onp.array(
         cc=cc,
-        data=matrix,
+        data=column_vector,
         batch_size=batch_size,
-        order=onp.ROW_MAJOR,
-        mode="zero",
-        fhe_type="C",
-        public_key=keys.publicKey,
-    )
-
-    cta_cm = onp.array(
-        cc=cc,
-        data=matrix,
-        batch_size=batch_size,
-        order=onp.COL_MAJOR,
+        order=order,
         mode="zero",
         fhe_type="C",
         public_key=keys.publicKey,
@@ -108,14 +104,23 @@ def example_broadcasting():
     # Broadcasting Vector
     ctv_mat1 = onp.broadcast_to(ctv, (5, 3), onp.ROW_MAJOR)
     res = ctv_mat1.decrypt(keys.secretKey, unpack_type="original")
-    print(f"\n Broadcast Vector -> Matrix:\n{res}")
+    print(f"\n Broadcast Vector -> Matrix (ROW_MAJOR):\n{res}")
 
     ctv_mat2 = onp.broadcast_to(ctv, (5, 3), onp.COL_MAJOR)
     res = ctv_mat2.decrypt(keys.secretKey, unpack_type="original")
-    print(f"\n Broadcast Vector -> Matrix:\n{res}")
+    print(f"\n Broadcast Vector -> Matrix (COL_MAJOR) :\n{res}")
+
+    # Broadcasting Vector Column
+    ctv_mat1 = onp.broadcast_to(ctv_col, (3, 5), onp.ROW_MAJOR)
+    res = ctv_mat1.decrypt(keys.secretKey, unpack_type="original")
+    print(f"\n Broadcast Column Vector-> Matrix (ROW_MAJOR):\n{res}")
+
+    ctv_mat2 = onp.broadcast_to(ctv_col, (3, 5), onp.COL_MAJOR)
+    res = ctv_mat2.decrypt(keys.secretKey, unpack_type="original")
+    print(f"\n Broadcast Column Vector-> Matrix (COL_MAJOR):\n{res}")
 
 
-def example_broadcasting_with_operations():
+def example_broadcasting_with_operations(ord=onp.ROW_MAJOR):
     """
     Example for scalar broadcasting
     """
@@ -134,7 +139,7 @@ def example_broadcasting_with_operations():
     keys = cc.KeyGen()
     cc.EvalMultKeyGen(keys.secretKey)
     cc.EvalSumKeyGen(keys.secretKey)
-    onp.gen_rotation_keys(keys.secretKey, list(range(-32, 32)))
+    # onp.gen_rotation_keys(keys.secretKey, list(range(-32, 32)))
 
     ring_dim = cc.GetRingDimension()
     batch_size = ring_dim // 2
@@ -142,19 +147,23 @@ def example_broadcasting_with_operations():
     print(f"Available slots:    {batch_size}")
 
     scalar = 5
-    vector = [10, 20, 30, 40, 50]
-    matrix = [[1.0, 2.0, 3.0, 4.0, 5.0], [1.0, 2.0, 3.0, 4.0, 5.0], [1.0, 2.0, 3.0, 4.0, 5.0]]
+    vector = [101, 210, 310, 140, 150]
+    column_vector = [[10], [20], [30]]
+    matrix = [[1.0, 2.0, 3.0, 4.0, 5.0], [1.1, 2.1, 3.1, 4.1, 5.1], [1.2, 2.2, 3.2, 4.2, 5.2]]
+
+    onp.generate_broadcast_key(keys.secretKey, (3, 5))
 
     print("\nInputs")
-    print("vector_a:", scalar)
-    print("vector_b:", vector)
-    print("vector_c:", matrix)
+    print("scalar:", scalar)
+    print("vector:", vector)
+    print("column_vector:", column_vector)
+    print("matrix:", matrix)
 
     cts = onp.array(
         cc=cc,
         data=scalar,
         batch_size=batch_size,
-        order=onp.ROW_MAJOR,
+        order=ord,
         mode="zero",
         fhe_type="C",
         public_key=keys.publicKey,
@@ -164,7 +173,17 @@ def example_broadcasting_with_operations():
         cc=cc,
         data=vector,
         batch_size=batch_size,
-        order=onp.ROW_MAJOR,
+        order=ord,
+        mode="zero",
+        fhe_type="C",
+        public_key=keys.publicKey,
+    )
+
+    ctv_col = onp.array(
+        cc=cc,
+        data=column_vector,
+        batch_size=batch_size,
+        order=ord,
         mode="zero",
         fhe_type="C",
         public_key=keys.publicKey,
@@ -174,14 +193,15 @@ def example_broadcasting_with_operations():
         cc=cc,
         data=matrix,
         batch_size=batch_size,
-        order=onp.ROW_MAJOR,
+        order=ord,
         mode="zero",
         fhe_type="C",
         public_key=keys.publicKey,
     )
 
     # Example for scalar + vector
-    ctv_add = cts + ctv
+    ctv_add = ctv + cts
+
     res_add = ctv_add.decrypt(keys.secretKey, unpack_type="original")
     validate_and_print_results(
         res_add,
@@ -205,6 +225,16 @@ def example_broadcasting_with_operations():
         f"Vector + Matrix  \n{scalar} \n{matrix}",
     )
 
+    # Example for column vector + matrix
+    ctv_add = ctm + ctv_col
+    res_add = ctv_add.decrypt(keys.secretKey, unpack_type="original")
+    validate_and_print_results(
+        res_add,
+        np.add(column_vector, matrix),
+        f"Column Vector + Matrix  \n{column_vector} \n{matrix}",
+    )
+
 
 if __name__ == "__main__":
+    # example_broadcasting()
     example_broadcasting_with_operations()

examples/python/simple_square_matrix_product.py
@@ -15,6 +15,7 @@ def validate_and_print_results(computed, expected, operation_name):
     return is_match, error
 
 
+# Matrix multiplication supports only tile mode and row-major encoding.
 def run_matmul_example(cc, keys, A, B, description, fhe_type=("C", "C")):
     """Run a homomorphic matrix x matrix multiplication example."""
     print(f"\n--- {description} ---")
@@ -27,7 +28,7 @@ def run_matmul_example(cc, keys, A, B, description, fhe_type=("C", "C")):
         cc=cc,
         data=A,
         batch_size=batch_size,
-        order=onp.COL_MAJOR,
+        order=onp.ROW_MAJOR,
         fhe_type=fhe_type[0],
         mode="tile",
         public_key=keys.publicKey,
@@ -37,7 +38,7 @@ def run_matmul_example(cc, keys, A, B, description, fhe_type=("C", "C")):
         cc=cc,
         data=B,
         batch_size=batch_size,
-        order=onp.COL_MAJOR,
+        order=onp.ROW_MAJOR,
         fhe_type=fhe_type[1],
         mode="tile",
         public_key=keys.publicKey,
@@ -109,12 +110,12 @@ def main():
             [3, 6, 10, 1, 2, 8, 4, 0],
         ]
     )
-    run_matmul_example(cc, keys, A8, B8, "8x8 Matrix Product (power-of-two)")
+    run_matmul_example(cc, keys, A8, B8, "8x8 Matrix Product (power-of-two)", ("C", "C"))
 
     # Case 2: non–power-of-two (3x3)
     A3 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     B3 = np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
-    run_matmul_example(cc, keys, A3, B3, "3x3 Matrix Product (non-power-of-two)", ("C", "P"))
+    run_matmul_example(cc, keys, A3, B3, "3x3 Matrix Product (non-power-of-two)", ("C", "C"))
     run_matmul_example(cc, keys, A3, B3, "3x3 Matrix Product (non-power-of-two)", ("P", "C"))
 
 

openfhe_numpy/operations/matrix_arithmetic.py
@@ -91,7 +91,7 @@ def add_ct(a, b):
         output_shape = np.broadcast_shapes(a.original_shape, b.original_shape)
         if a.original_shape != output_shape:
             a = broadcast_to(a, output_shape, a.order)
-        if b.original_shape != output_shape:
+        else:
             b = broadcast_to(b, output_shape, b.order)
         return _eval_add(a, b)
 
@@ -188,7 +188,6 @@ def _eval_multiply(lhs, rhs):
         rhs_data = rhs.data
 
     result = crypto_context.EvalMult(lhs.data, rhs_data)
-    a = lhs.clone(result)
     return lhs.clone(result)
 
 
@@ -243,7 +242,6 @@ def multiply_block_ct_scalar(a, scalar):
 def _eval_matvec_ct(lhs, rhs):
     """Internal function to evaluate matrix-vector multiplication."""
     cc = rhs.data.GetCryptoContext() if rhs.dtype == "CTArray" else lhs.data.GetCryptoContext()
-
     if lhs.ndim == 2 and rhs.ndim == 1:
         if lhs.original_shape[1] != rhs.original_shape[0]:
             ONPIncompatibleShape(

openfhe_numpy/utils/packing.py
@@ -544,18 +544,24 @@ def _extract_matrix(data, info):
         nrows = info["batch_size"] // ncols
         reshaped = np.reshape(data, (nrows, ncols))
         tranposed = np.transpose(reshaped)
-        # print("DEBUG ::: reshaped", reshaped)
-        # print("DEBUG ::: tranposed", tranposed)
         return tranposed[: info["original_shape"][0], : info["original_shape"][1]]
     else:
         ONP_ERROR("Order is not supported!!!")
         return None
 
 
-# @validate_call
+# Function _extract_vector recovers the packed vector in "zero" or "tile" mode
+# 1. mode "zero"
+#   vector of [a,b,c] is packed as [a,b,c]
+# 2. mode "tiles"
+#   an (m,) vector is extended to mxn matrix with a new shape of (m, n)
+#   [a,b,c]
+#   [a,b,c]
+#   RM packing is : a b c 0 | a b c 0
+#   CM packing is : a a | b b | c c |0 0
 def _extract_vector(data, info):
     if info["ndim"] == 1:
-        if len(info["shape"]) == 1:
+        if len(info["shape"]) == 1 or info["shape"][1] == 1:
             return data[: info["original_shape"][0]]
         else:
             original_row = info["original_shape"][0]
@@ -565,7 +571,6 @@ def _extract_vector(data, info):
         reshaped = np.reshape(data, (nrows, ncols))
 
         if info["order"] == ROW_MAJOR:
-            # get [original_row] elements from the first columns
             return reshaped[:original_row, 0]
         elif info["order"] == COL_MAJOR:
             return reshaped[0, :original_row]
@@ -596,7 +601,6 @@ def process_packed_data(
     ndarray
         Reshaped array to its desired shape
     """
-
     if info["ndim"] == 2:
         return _extract_matrix(data, info)
     else: